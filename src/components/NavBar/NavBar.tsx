2dd62a8d560f759b4106107666808a16db60dd36
dreamc4t <eriksund1@gmail.com>
2024-08-22 21:06:09 +0200

    auth button navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,23 +1,23 @@
+import { AuthButton } from '@/components/AuthButton'
 import { navigationItems } from './navigationItems'
 import Link from 'next/link'
 
 const NavBar = () => {
   return (
-    <div className=''>
-      <nav className='flex p-2 gap-5'>
-        {navigationItems.map((item, i) => {
-          return (
-            <Link
-              href={item.url}
-              key={item.title + i}
-              className='text-gray-200 hover:text-yellow-400 text-sm md:text-lg font-medium py-1 md:py-2 px-3 md:px-4 bg-gray-700 bg-opacity-60 hover:bg-opacity-70 rounded-md transition duration-300 ease-in-out shadow-md hover:shadow-lg'
-            >
-              {item.title}
-            </Link>
-          )
-        })}
-      </nav>
-    </div>
+    <nav className='flex p-2 gap-5'>
+      {navigationItems.map((item, i) => {
+        return (
+          <Link
+            href={item.url}
+            key={item.title + i}
+            className='text-gray-200 hover:text-yellow-400 text-sm md:text-lg font-medium py-1 md:py-2 px-3 md:px-4 bg-gray-700 bg-opacity-60 hover:bg-opacity-70 rounded-md transition duration-300 ease-in-out shadow-md hover:shadow-lg'
+          >
+            {item.title}
+          </Link>
+        )
+      })}
+      <AuthButton />
+    </nav>
   )
 }
 

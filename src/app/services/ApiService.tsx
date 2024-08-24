c62de958958bdd5c6a03b66a0cb3970e10892cee
dreamc4t <eriksund1@gmail.com>
2024-08-24 17:18:12 +0200

    apiservice

--- src/app/services/ApiService.tsx
+++ src/app/services/ApiService.tsx
@@ -1,11 +1,25 @@
-import axios, { AxiosRequestConfig } from 'axios'
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
 
 class ApiServiceClass {
-  baseURL = process.env.NEXT_PUBLIC_BACKEND_URL
+  private axiosInstance: AxiosInstance
+
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    })
+  }
 
   public async get<T>(endpoint: string, config?: AxiosRequestConfig): Promise<T> {
-    const url = `${this.baseURL}/${endpoint}`
-    const res = await axios.get(url, config)
+    const res = await this.axiosInstance.get<T>(endpoint, config)
+    return res.data
+  }
+
+  public async post<T>(
+    endpoint: string,
+    data: any,
+    config?: AxiosRequestConfig,
+  ): Promise<T> {
+    const res = await this.axiosInstance.post<T>(endpoint, data, config)
     return res.data
   }
 }

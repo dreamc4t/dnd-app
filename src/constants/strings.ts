7239262a1a8b43bfe11fd8c4d0070ac57ce2c5d5
dreamc4t <eriksund1@gmail.com>
2024-08-27 08:49:03 +0200

    strings

---
+++ src/constants/strings.ts
@@ -0,0 +1,23 @@
+// const addString = 'add' as const
+// const alreadyHaveAnAccountString = 'Already have an account?' as const
+// const filtersString = 'Filters' as const
+// const noActiveShopString = 'No active shop' as const
+// const registerString = 'Register' as const
+// const registrationFormString = 'Registration Form' as const
+// const removeString = 'remove' as const
+// const saveShopString = 'Save shop' as const
+const searchForItemsString = 'Search for items...' as const
+// const weightString = 'Weight' as const
+
+export {
+  // addString,
+  // alreadyHaveAnAccountString,
+  // filtersString,
+  // noActiveShopString,
+  // registerString,
+  // registrationFormString,
+  // removeString,
+  // saveShopString,
+  searchForItemsString,
+  // weightString,
+}

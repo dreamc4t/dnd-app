14ca856b05b853d5a7e05f31f7c07e9c6a4504a0
dreamc4t <eriksund1@gmail.com>
2024-09-28 18:24:34 +0200

    function called on cliick tested

--- src/components/AuthButton/SignInButton/SignInButton.test.tsx
+++ src/components/AuthButton/SignInButton/SignInButton.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { SignInButton } from './SignInButton'
 import { signInString } from '@/constants/strings'
+import { signIn } from 'next-auth/react'
 
 jest.mock('next-auth/react', () => ({
   signIn: jest.fn(),
@@ -11,5 +12,12 @@ describe('SignIn Button', () => {
     render(<SignInButton />)
     const button = screen.getByRole('button', { name: signInString })
     expect(button).toBeInTheDocument()
+  })
+
+  it('fires signIn function when clicked', () => {
+    render(<SignInButton />)
+    const button = screen.getByRole('button', { name: signInString })
+    fireEvent.click(button)
+    expect(signIn).toHaveBeenCalled()
   })
 })
--- src/components/AuthButton/SignOutButton/SignOutButton.test.tsx
+++ src/components/AuthButton/SignOutButton/SignOutButton.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { SignOutButton } from './SignOutButton'
 import { signOutString } from '@/constants/strings'
+import { signOut } from 'next-auth/react'
 
 jest.mock('next-auth/react', () => ({
   signOut: jest.fn(),
@@ -11,5 +12,12 @@ describe('Sign out Button', () => {
     render(<SignOutButton />)
     const button = screen.getByRole('button', { name: signOutString })
     expect(button).toBeInTheDocument()
+  })
+
+  it('fires signOut function when clicked', () => {
+    render(<SignOutButton />)
+    const button = screen.getByRole('button', { name: signOutString })
+    fireEvent.click(button)
+    expect(signOut).toHaveBeenCalled()
   })
 })

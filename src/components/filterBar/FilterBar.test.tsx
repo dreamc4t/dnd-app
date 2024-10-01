906b2bf04fb8b3b6e7a6ef5fc5859aee8dcdc6ac
Erik Sund <erik.sund@cygni.se>
2024-10-01 17:00:05 +0200

    filterbar test updated

--- src/components/filterBar/FilterBar.test.tsx
+++ src/components/filterBar/FilterBar.test.tsx
@@ -4,17 +4,30 @@ import { render, screen } from '@testing-library/react'
 
 const mockFilters = ['black', 'green', 'white']
 const mockSetFilter = jest.fn()
+const mockSelectedFilters = ['black', 'green']
 
 describe('FilterBar', () => {
   it('renders component', () => {
-    render(<FilterBar filters={mockFilters} setFilter={mockSetFilter} />)
+    render(
+      <FilterBar
+        filters={mockFilters}
+        setFilter={mockSetFilter}
+        selectedFilters={mockSelectedFilters}
+      />,
+    )
 
     const bar = screen.getByRole('group')
     expect(bar).toBeInTheDocument()
   })
 
   it('renders buttons for every filter', () => {
-    render(<FilterBar filters={mockFilters} setFilter={mockSetFilter} />)
+    render(
+      <FilterBar
+        filters={mockFilters}
+        setFilter={mockSetFilter}
+        selectedFilters={mockSelectedFilters}
+      />,
+    )
 
     const buttons = screen.getAllByRole('button')
     expect(buttons).toHaveLength(mockFilters.length)

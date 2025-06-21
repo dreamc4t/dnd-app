ee6cbbc2335da14f8c9d9e9ea0458b6549fac061
dreamc4t <eriksund1@gmail.com>
2025-06-21 13:11:46 +0200

    appTitle as const

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link'
 import { Navigation } from '../Navigation'
 import { ProfileOrSigninButton } from '../ProfileOrSignInButton'
+import { appTitle } from '@/constants/strings'
 
 const Header = () => {
   return (
     <header className='z-10 border-b'>
       <div className='mx-5 flex h-14 items-center justify-between'>
         {/* TODO Separate into logo */}
-        <Link className='flex items-center' href={'/'}>
-          <h1 className='font-semibold'>D&D Shop Creator</h1>
+        <Link className='flex items-center font-semibold' href={'/'}>
+          {appTitle}
         </Link>
 
         <div className='flex items-center gap-8'>

80279d821703a9aec1e25f5889e7626864152e5e
dreamc4t <eriksund1@gmail.com>
2025-06-21 13:03:13 +0200

    css fixes header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,17 @@
+import Link from 'next/link'
 import { Navigation } from '../Navigation'
 import { ProfileOrSigninButton } from '../ProfileOrSignInButton'
 
 const Header = () => {
   return (
-    <header className='z-10 h-16'>
-      <div className='flex items-center justify-between border border-red-600'>
-        <div className='flex items-center'>
-          <span className='font-semibold'>D&D Shop Creator</span>
-        </div>
+    <header className='z-10 border-b'>
+      <div className='mx-5 flex h-14 items-center justify-between'>
+        {/* TODO Separate into logo */}
+        <Link className='flex items-center' href={'/'}>
+          <h1 className='font-semibold'>D&D Shop Creator</h1>
+        </Link>
 
-        <div className='flex items-center'>
+        <div className='flex items-center gap-8'>
           <Navigation />
           <ProfileOrSigninButton />
         </div>
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,7 +3,7 @@ import { NavList } from './NavList'
 
 const Navigation = () => {
   return (
-    <nav className='flex items-center justify-between p-4'>
+    <nav>
       <NavList navigationItems={navigationItems} />
     </nav>
   )

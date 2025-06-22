83a16a98de5d5e160412b61320cab06be5d02a29
dreamc4t <eriksund1@gmail.com>
2025-06-22 09:33:47 +0200

    sort strings

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,46 +1,47 @@
-const filtersString = 'Filters' as const
-const saveShopString = 'Save shop' as const
-const searchForItemsString = 'Search for items...' as const
-const descriptionString = 'Description: ' as const
-const noItemsFoundString = 'No items found' as const
-const noItemsSelectedString = 'No items selected' as const
-const enterShopNameString = 'Enter shop name...' as const
-const noShopsFoundsString = 'No shops found' as const
-const isSavingString = 'Saving...' as const
-const signInString = 'Sign in' as const
-const signOutString = 'Sign out' as const
 const addToShopString = 'Add to shop' as const
-const removeString = 'Remove' as const
-const deleteString = 'Delete' as const
-const notLoggedInString = 'You are not logged in' as const
-const mySavedShopsTitle = 'My Shops' as const
 const appTitle = 'D&D Shop Creator' as const
 const createdAtString = 'Created at' as const
+const deleteString = 'Delete' as const
+const descriptionString = 'Description: ' as const
+const enterShopNameString = 'Enter shop name...' as const
+const filtersString = 'Filters' as const
+const isSavingString = 'Saving...' as const
 const itemsString = 'items' as const
+const mySavedShopsTitle = 'My Shops' as const
+const noItemsFoundString = 'No items found' as const
+const noItemsSelectedString = 'No items selected' as const
+const noShopsFoundsString = 'No shops found' as const
+const notLoggedInString = 'You are not logged in' as const
+const removeString = 'Remove' as const
+const saveShopString = 'Save shop' as const
+const searchForItemsString = 'Search for items...' as const
+const signInString = 'Sign in' as const
+const signOutString = 'Sign out' as const
 
 // Errors
 const noItemErrorMessage = 'You must add at least one item' as const
 const noShopNameErrorMessage = 'Missing shop name' as const
+
 export {
-  signInString,
   addToShopString,
+  appTitle,
   createdAtString,
-  filtersString,
-  signOutString,
-  itemsString,
+  deleteString,
+  descriptionString,
   enterShopNameString,
-  saveShopString,
-  searchForItemsString,
+  filtersString,
+  isSavingString,
+  itemsString,
+  mySavedShopsTitle,
+  noItemErrorMessage,
   noItemsFoundString,
   noItemsSelectedString,
-  noShopsFoundsString,
-  descriptionString,
-  isSavingString,
-  deleteString,
-  removeString,
-  noItemErrorMessage,
   noShopNameErrorMessage,
+  noShopsFoundsString,
   notLoggedInString,
-  mySavedShopsTitle,
-  appTitle,
+  removeString,
+  saveShopString,
+  searchForItemsString,
+  signInString,
+  signOutString,
 }

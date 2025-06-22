705a024debbd09c6dc1dc552629f46dfa3dec4dc
dreamc4t <eriksund1@gmail.com>
2025-06-22 09:52:31 +0200

    nav items as consts

--- src/components/Navigation/navigationItems.ts
+++ src/components/Navigation/navigationItems.ts
@@ -1,10 +1,12 @@
+import { NAV_CREATE_SHOP_URL, NAV_MY_SHOPS_URL, NAV_NPC_URL } from '@/constants/urls'
+
 export type NavigationItem = {
   url: string
   title: string
 }
 
 export const navigationItems: NavigationItem[] = [
-  { title: 'NPC', url: '/npc' },
-  { title: 'Create Shop', url: '/shop-builder' },
-  { title: 'My shops', url: '/my-shops' },
+  { title: 'NPC', url: `/${NAV_NPC_URL}` },
+  { title: 'Create Shop', url: `/${NAV_CREATE_SHOP_URL}` },
+  { title: 'My shops', url: `/${NAV_MY_SHOPS_URL}` },
 ]

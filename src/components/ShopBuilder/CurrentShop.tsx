39763975b82d85d2d0c8a3e816822fcb72b02a80
dreamc4t <eriksund1@gmail.com>
2025-07-18 09:38:28 +0200

    shop name input better

--- src/components/ShopBuilder/CurrentShop.tsx
+++ src/components/ShopBuilder/CurrentShop.tsx
@@ -5,6 +5,7 @@ import { ShopNameInput } from './ShopNameInput'
 import { useShopBuilderContext } from './ShopBuilderContext'
 import { LoadingSpinnerOverlay } from '../LoadingSpinnerOverlay'
 import { useItemsContext } from '@/context'
+import { Heading } from '../Heading'
 
 export const CurrentShop = () => {
   const { removeItemFromShop, selectedItems, isSaving, errorMessage, updateItemInShop } =
@@ -13,11 +14,10 @@ export const CurrentShop = () => {
 
   return (
     <div className={`relative flex h-full flex-col ${isSaving ? 'blur-sm' : ''}`}>
+      <Heading variant='h2' title='Current Shop' className='mb-3' />
       <div>
-        <div className='flex items-center space-x-4'>
-          <ShopNameInput />
-        </div>
-        {errorMessage && <p className='absolute text-sm text-red-500'>{errorMessage}</p>}
+        <ShopNameInput />
+        {errorMessage && <p className='text-sm text-red-500'>{errorMessage}</p>}
       </div>
       <div className='flex-grow overflow-auto'>
         <ItemsList
--- src/components/ShopBuilder/ShopNameInput.tsx
+++ src/components/ShopBuilder/ShopNameInput.tsx
@@ -1,16 +1,54 @@
+'use client'
+
+import { useState, useRef, useEffect } from 'react'
+import { Heading } from '../Heading'
+import { EditIcon } from '../icons' // adjust as needed
 import { enterShopNameString } from '@/constants/strings'
 import { useShopBuilderContext } from './ShopBuilderContext'
 
 export const ShopNameInput = () => {
   const { shopName, setShopName } = useShopBuilderContext()
+  const [isEditing, setIsEditing] = useState(false)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [isEditing])
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      setIsEditing(false)
+    }
+  }
 
   return (
-    <input
-      type='text'
-      value={shopName}
-      onChange={(e) => setShopName(e.target.value)}
-      placeholder={enterShopNameString}
-      className='flex-grow rounded border border-gray-300 p-2'
-    />
+    <div className='mb-4 flex items-center gap-1'>
+      <label className='text-nowrap text-text-secondary'>Shop name:</label>
+      <div className='flex w-full items-center gap-1' onClick={() => setIsEditing(true)}>
+        {isEditing ? (
+          <input
+            ref={inputRef}
+            type='text'
+            value={shopName}
+            onChange={(e) => setShopName(e.target.value)}
+            onBlur={() => setIsEditing(false)}
+            onKeyDown={handleKeyDown}
+            placeholder={enterShopNameString}
+            className='w-full rounded bg-backgroundTint2 font-semibold text-text-primary focus:outline-none'
+          />
+        ) : (
+          <p className='cursor-pointer font-semibold text-text-primary'>
+            {shopName || (
+              <span className='italic text-text-secondary'>{enterShopNameString}</span>
+            )}
+          </p>
+        )}
+        {shopName && !isEditing && (
+          <EditIcon className='cursor-pointer fill-text-primary' size={16} />
+        )}
+      </div>
+    </div>
   )
 }

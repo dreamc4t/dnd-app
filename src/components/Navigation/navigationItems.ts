2d46edf873ed6b0d1040e5f4e2b93773dc1cb7db
dreamc4t <eriksund1@gmail.com>
2025-10-05 16:58:46 +0200

    npc nav commented out

--- src/components/Navigation/navigationItems.ts
+++ src/components/Navigation/navigationItems.ts
@@ -1,4 +1,5 @@
-import { NAV_CREATE_SHOP_URL, NAV_MY_SHOPS_URL, NAV_NPC_URL } from '@/constants/urls'
+// import { NAV_CREATE_SHOP_URL, NAV_MY_SHOPS_URL, NAV_NPC_URL } from '@/constants/urls'
+import { NAV_CREATE_SHOP_URL, NAV_MY_SHOPS_URL } from '@/constants/urls'
 
 export type NavigationItem = {
   url: string
@@ -6,7 +7,7 @@ export type NavigationItem = {
 }
 
 export const navigationItems: NavigationItem[] = [
-  { title: 'NPC', url: `/${NAV_NPC_URL}` },
+  // TODO REIMPLEMENT WHEN READY { title: 'NPC', url: `/${NAV_NPC_URL}` },
   { title: 'Create Shop', url: `/${NAV_CREATE_SHOP_URL}` },
   { title: 'My shops', url: `/${NAV_MY_SHOPS_URL}` },
 ]
